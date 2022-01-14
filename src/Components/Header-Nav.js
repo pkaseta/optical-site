aa658050ddb35648c9e7bf958fa60f08b829a8fa
Paul Kaseta <pkaseta@redstonecontentsolutions.com>
2022-01-13 20:02:28 -0500

    initial commit

---
+++ src/Components/Header-Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from "../Assets/logo.png"
+
+const HeaderNav = () => {
+    return (
+      <div className="navbar-container">
+        <div className="logo-container">
+          <img src={logo} />
+        </div>
+        <div className="company-title-home-nav"></div>
+        <div className="site-links-container"></div>
+      </div>
+    )
+}
+
+export default HeaderNav
