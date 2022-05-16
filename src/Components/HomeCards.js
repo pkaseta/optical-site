9d90cc163795b4b93e0d1303774d82091130c6a1
Paul Kaseta <kasetapaul@gmail.com>
2022-05-16 14:27:24 -0400

    Auto stash before merge of "main" and "origin/main"

---
+++ src/Components/HomeCards.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+
+const HomeCards = (props) => {
+  return (
+    <div className="card-container">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={props.image} />
+        <Card.Body>
+          <Card.Title>{props.frameName}</Card.Title>
+          <Card.Text>{props.color}</Card.Text>
+          <Button className="add-to-cart" variant="dark">Add to cart</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default HomeCards;
